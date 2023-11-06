835d69148ba5db4728139b0c9e3ec5802e0a65ea
Sebastian Leonarski <sleonarski@alan-systems.com>
2023-11-06 14:06:15 +0100

    Day 3 first commit WIP

--- src/main/java/org/example/day3/Main.java
+++ src/main/java/org/example/day3/Main.java
@@ -1,60 +1,8 @@
 package org.example.day3;
 
-import org.example.common.utils.DataReader;
-
-import java.util.List;
-
 public class Main {
 
     public static void main(String[] args) {
-
-        String testData = """
-                00100
-                11110
-                10110
-                10111
-                10101
-                01111
-                00111
-                11100
-                10000
-                11001
-                00010
-                01010
-                """;
-
-        StringBuilder gamma = new StringBuilder();
-        StringBuilder epsilon = new StringBuilder();
-
-        List<String> diagnosticReport = DataReader.readFileAsList("src/main/resources/day3/input.txt"); // 3813416
-//        List<String> diagnosticReport = testData.lines().toList();
-
-        for (int i = 0; i < diagnosticReport.get(0).length(); i++) {
-            int zeros = 0;
-            int ones = 0;
-            for (String s : diagnosticReport) {
-                if (s.charAt(i) == '0') {
-                    zeros++;
-                } else {
-                    ones++;
-                }
-            }
-            if (ones > zeros) {
-                gamma.append("1");
-                epsilon.append("0");
-            } else {
-                gamma.append("0");
-                epsilon.append("1");
-            }
-        }
-        System.out.println(convert(gamma) * convert(epsilon));
-    }
-
-    private static int convert(StringBuilder data) {
-        int convertedData = 0;
-        for (char c : data.toString().toCharArray()) {
-            convertedData = c == '0' ? convertedData << 1 : convertedData << 1 | 1;
-        }
-        return convertedData;
+        //TODO run part one
     }
 }
---
+++ src/test/java/org/example/day3/ReportDecoderTest.java
@@ -0,0 +1,23 @@
+package org.example.day3;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ReportDecoderTest {
+
+    private static final String TEST_DATA = """
+            00100
+            11110
+            10110
+            10111
+            10101
+            01111
+            00111
+            11100
+            10000
+            11001
+            00010
+            01010
+            """;
+
+//TODO write testcases
+}
