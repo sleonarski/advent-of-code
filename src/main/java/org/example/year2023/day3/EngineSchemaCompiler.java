818174fce3b51f72508b3d06235c844b225e8b53
Leo <sed@interia.eu>
2023-12-11 23:59:22 +0100

    day3/partOne - changed impl

--- src/main/java/org/example/year2023/day3/EngineSchemaCompiler.java
+++ src/main/java/org/example/year2023/day3/EngineSchemaCompiler.java
@@ -1,9 +1,9 @@
 package org.example.year2023.day3;
 
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
+import java.util.function.BinaryOperator;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 import static java.util.stream.Collectors.groupingBy;
 import static java.util.stream.Collectors.toMap;
@@ -12,41 +12,30 @@ public class EngineSchemaCompiler {
     public int calculate(String testInput) {
 
         List<String> slicedInput = Arrays.stream(testInput.split("\n")).toList();
-        List<RowCompiler> instructionRowsList = slicedInput.stream().map(RowCompiler::new).toList();
 
-        Map<Integer, String> integerCharacterMap = checkEngineParts(instructionRowsList);
-
-        instructionRowsList.stream().forEach(row -> {
-            System.out.println();
-            row.getRowCharacters().forEach(c -> System.out.print(c));
-        });
-
+        List<List<Integer>> list = slicedInput.stream()
+                .map(row -> getParts(row)).toList();
 
         return 0;
     }
 
-    private Map<Integer, String> checkEngineParts(List<RowCompiler> instructionRowsList) {
-        for (RowCompiler compiler : instructionRowsList) {
-            for (String charr : compiler.getRowCharacters()) {
-                if (!charr.equals(".")) {
-                    // znalezc index of charr, dodac od listy, sprawdzac indexy miedzy rowami
-                }
-            }
-        }
+    private List<Integer> getParts(String row) {
+        List<String> rowsList = Arrays.stream(row.split("")).toList();
 
-        private Map<Integer, String> checkEngineParts2 (List < RowCompiler > instructionRowsList) {
+        List<Integer> list = IntStream.range(0, rowsList.size())
+                .filter(i -> isPart(rowsList.get(i)))
+                .boxed()
+                .toList();
 
-            Map<Integer, String> map = new HashMap<>();
-            for (RowCompiler row : instructionRowsList) {
-                List<String> rowCharacters = row.getRowCharacters();
-                for (int i = 0; i < rowCharacters.size(); i++) {
-                    String v = rowCharacters.get(i);
-                    System.out.println(v);
-                    if (!v.equals(".")) {
-                        map.put(i, v);
-                    }
-                }
-            }
-            return map;
-        }
+        return list;
     }
+
+    private static boolean isPart(String symbol) {
+        return !symbol.equals(".");
+    }
+
+    private boolean isNumber(Object i) {
+        return true;
+    }
+
+}
